e89f9bc96fd5a70a0e9f103faa53cfc986de11b9
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-23 07:41:27 +0100

    first commit

---
+++ src/app/data.tsx
@@ -0,0 +1,20 @@
+interface Data {
+    city: string;
+    state: string;
+    brand: string;
+    domain: string;
+    logo: string;
+    src: string;
+}
+
+const data:Data ={
+    brand:"RHS",
+    city:"Boca Raton",
+    state:"FL",
+    domain: "bocaratonherniaspecialists.com",
+    logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719124349/RHS_ueejin.png',
+    src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57193.09180204801!2d-80.15856517299511!3d26.372823504846632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d923c2ef36fb27%3A0xd23ea6eeca6095c9!2sBoca%20Raton%2C%20FL%2C%20USA!5e0!3m2!1sen!2sng!4v1719124777028!5m2!1sen!2sng"
+
+}
+
+export default data
